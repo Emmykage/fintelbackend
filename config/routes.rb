7c1b89d91a183ad51054b0a3ad40306028ddaf39
morris menanya <emmiemenz@gmail.com>
2024-04-07 16:14:07 +0100

    transaction medel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :transactions
   resources :wallets
   # post '/login', to: "users#login"
 
---
+++ db/migrate/20240407151333_create_transactions.rb
@@ -0,0 +1,14 @@
+class CreateTransactions < ActiveRecord::Migration[7.1]
+  def change
+    create_table :transactions, id: :uuid do |t|
+      t.decimal :amount
+      t.integer :transaction_type
+      t.integer :status
+      t.string :address
+      t.integer :coin_type
+      t.references :wallet, null: false, foreign_key: true, type: :uuid
+
+      t.timestamps
+    end
+  end
+end
