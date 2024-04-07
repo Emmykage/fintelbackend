f4e4007cb8c65eda39b5d45778e04cfa5f605676
morris menanya <emmiemenz@gmail.com>
2024-04-07 16:13:19 +0100

    wallet model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :wallets
   # post '/login', to: "users#login"
 
   namespace :api do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_06_095342) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_07_150320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -30,4 +30,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_06_095342) do
     t.string "confirmation_token"
   end
 
+  create_table "wallets", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.decimal "balance"
+    t.uuid "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id"
+  end
+
+  add_foreign_key "wallets", "users"
 end
