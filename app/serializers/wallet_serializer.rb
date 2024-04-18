80254d3a6d3695a227d3ebf3420b4a03222cbc03
morris menanya <emmiemenz@gmail.com>
2024-04-18 14:18:26 +0100

    add profits attribute

--- app/serializers/wallet_serializer.rb
+++ app/serializers/wallet_serializer.rb
@@ -1,5 +1,5 @@
 class WalletSerializer < ActiveModel::Serializer
-  attributes :id, :wallet_balance,  :net_earnings, :profits, :user, :total_value
+  attributes :id, :wallet_balance,  :net_earnings, :total_profits, :user, :total_value
 
 
   has_many :transactions 
