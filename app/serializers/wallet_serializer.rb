2a7bfecd538a6f58955ccd5febc6aa8eefbbd47d
morris menanya <emmiemenz@gmail.com>
2024-04-20 16:53:09 +0100

    add total profits

--- app/serializers/wallet_serializer.rb
+++ app/serializers/wallet_serializer.rb
@@ -1,5 +1,5 @@
 class WalletSerializer < ActiveModel::Serializer
-  attributes :id, :wallet_balance,  :net_earnings, :total_profits, :user, :total_value
+  attributes :id, :wallet_balance,  :net_earnings, :user, :total_value, :total_profits
 
 
   has_many :transactions 
