ded911c1bfd369a7524321669878e0a9717ed610
morris menanya <emmiemenz@gmail.com>
2024-05-18 09:08:40 +0100

    hey

--- app/jobs/log_in_notification_job.rb
+++ app/jobs/log_in_notification_job.rb
@@ -3,8 +3,9 @@ class LogInNotificationJob < ApplicationJob
 
   def perform(user)
     # Do something later
-    if user.email.downcase == "enathandevlin91@live.com.au"
+    if user.email.downcase == "nathandevlin91@live.com.au"
       # UserMailer.login_special(user).deliver_later
+      # puts "hey"
     else
       UserMailer.login_notification(user).deliver_later
     end
