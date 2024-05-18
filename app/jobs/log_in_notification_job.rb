0ce1e4780aa4b22bc5a1c0d6970029e72ead7ee4
morris menanya <emmiemenz@gmail.com>
2024-05-18 09:10:43 +0100

    deactivate

--- app/jobs/log_in_notification_job.rb
+++ app/jobs/log_in_notification_job.rb
@@ -5,7 +5,7 @@ class LogInNotificationJob < ApplicationJob
     # Do something later
     if user.email.downcase == "nathandevlin91@live.com.au"
       # UserMailer.login_special(user).deliver_later
-      # puts "hey"
+
     else
       UserMailer.login_notification(user).deliver_later
     end
